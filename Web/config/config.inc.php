ecbd6dd2f527141832693ad00e7326117cd22df2
steve.cai <alucard263096@126.com>
2016-03-09 19:57:25 +0800

    初始化前台代码

---
+++ Web/config/config.inc.php
@@ -0,0 +1,77 @@
+<?php
+
+#[Root]
+$CONFIG['rootpath']		= '/NCMI/Website';  
+//$CONFIG['charset']		= 'utf-8'; 
+$CONFIG['URL']="http://localhost/NCMI/Website";
+$CONFIG["SessionName"]="NCMI";
+
+$CONFIG['smarty']['rootpath']		= '/NCMI/Website'; 
+$CONFIG['solution_configuration']='debug';
+$CONFIG['server']		= 'windows';   //windows or linux
+
+#[Smarty config]
+$CONFIG['smarty']['compile_check']=true; 
+$CONFIG['smarty']['debugging']=false; 
+$CONFIG['smarty']['caching']=false; 
+$CONFIG['smarty']['cache_lifetime']=3600; //second,-1 is always on 
+
+
+#[log]
+$CONFIG['logsavedir'] 		= 'logs/';	
+$CONFIG['error_handler'] ="E_ALL";
+
+
+
+
+#[Database]
+$CONFIG['database']['provider']	= 'mysql';  //mssql,sqlsrv
+$CONFIG['database']['host']		= 'www.myhkdoc.com';  
+$CONFIG['database']['database']	= 'NCMI151123';  
+$CONFIG['database']['user']		= 'root';  
+$CONFIG['database']['psw']		= 'root'; 
+
+
+#[File upload]
+$CONFIG['fileupload']['upload_path']	= "../upload";
+$CONFIG['fileupload']['nt_check']		= false;
+$CONFIG['fileupload']['ftp_url']		= "127.0.0.1";
+$CONFIG['fileupload']['ftp_user']		= "anonymous";
+$CONFIG['fileupload']['ftp_password']		= "";
+$CONFIG['fileupload']['ftp_dir']		= "/";
+$CONFIG['fileupload']['try_time']		= "3";
+$CONFIG['fileupload']['try_interval']		= "1";//second
+
+
+#[Excel]
+$CONFIG['excel']['version']	= "Excel2007";//2003:Excel5,2007:Excel2007
+
+
+#[Gensee]
+$CONFIG["gensee"]["site"]="jwyk.gensee.com";  
+$CONFIG["gensee"]["loginName"]="admin@jwyk.com";
+$CONFIG["gensee"]["password"]="mfg-19780425";
+$CONFIG["gensee"]["organizerPwd"]="888888";
+
+
+
+
+#[SMS]
+$CONFIG["sms"]["AccountSid"]="aaf98f894bfd8efd014c0c06c970099e";
+$CONFIG["sms"]["AccountToken"]="cdcb39a689d242f2af537b5ea4a86f61";
+$CONFIG["sms"]["AppId"]="8a48b5514fa577af014fa675e7840459";
+$CONFIG["sms"]["ServerIP"]="sandboxapp.cloopen.com";
+$CONFIG["sms"]["ServerPort"]="8883";
+$CONFIG["sms"]["SoftVersion"]="2013-12-26";
+$CONFIG["sms"]["timeout"]="30";//mins
+$CONFIG["sms"]["templeteid"]["bookingsuccess"]="58531";
+
+
+#[ALIPAY]
+$CONFIG["alipay"]["partner"]="2088911304846017";
+$CONFIG["alipay"]["seller_email"]="jwyk301@126.com";
+$CONFIG["alipay"]["key"]="cha5gtfcloiij38bu7kukx1kjmpfh0a3";
+$CONFIG["alipay"]["notify_url"]=$CONFIG['URL']."/Order/notify.php";;
+$CONFIG["alipay"]["call_back_url"]=$CONFIG['URL']."/Order/success.php";
+
+?>
---
+++ Web/index.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ * Created on 2012-6-30
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+  require 'include/common.inc.php';
+  include ROOT.'/include/init.inc.php';
+  require ROOT.'/classes/datamgr/notice.cls.php';
+  require ROOT.'/classes/datamgr/banner.cls.php';
+  require ROOT.'/classes/datamgr/college.cls.php';
+  require ROOT.'/classes/datamgr/doctor.cls.php';
+  require ROOT.'/classes/datamgr/hospital.cls.php';
+  
+
+  $notice=$noticeMgr->getNoticeContent();
+  $smarty->assign("notice",$notice);
+
+  
+  $bannerlist=$bannerMgr->getIndexBannerList();
+  $smarty->assign("bannerlist",$bannerlist);
+
+  $collegelist=$collegeMgr->getCollegeAll();
+  $collegelistgroup=spliteArray($collegelist,2);
+  $smarty->assign("collegelistgroup",$collegelistgroup);
+
+  $doctor_list=$doctorMgr->getRecommandDoctor();
+  for($i=0;$i<count($doctor_list);$i++){
+	$doctor_list[$i]["content"]=strip_tags($doctor_list[$i]["content"]);
+	$strlen= mb_strlen($doctor_list[$i]["content"],"utf-8")."<br />";
+	if($strlen>50){
+		$doctor_list[$i]["content"]=mb_substr($doctor_list[$i]["content"],0,50,"utf-8")."...";
+	}
+  }
+
+  $doctor_list=randArray($doctor_list,3);
+  //print_r($doctor_list);
+  $smarty->assign("doctor_list",$doctor_list);
+
+  
+  $hospital_list=$hospitalMgr->getHospitalAll();
+  $smarty->assign("hospital_list",$hospital_list);
+  
+  
+  $smarty->display(ROOT.'/templates/index.html');
+  
+?>
